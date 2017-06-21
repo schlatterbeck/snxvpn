a8253d28db130a1a4dee496344380c2597f5dbbb
Ralf Schlatterbeck <rsc@runtux.com>
2017-06-21 23:41:25 +0200

    Add password encoding
    This works in tests against the javascript implementation but doesn't
    produce a valid login so far.

--- snxconnect.py
+++ snxconnect.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import os
 import sys
 import urllib2
 from rsclib.HTML_Parse import Page_Tree
@@ -9,6 +10,7 @@ from getpass           import getpass
 from urllib            import urlencode
 from argparse          import ArgumentParser
 from netrc             import netrc
+from Crypto.PublicKey  import RSA
 
 class HTML_Requester (object) :
 
@@ -38,12 +40,16 @@ class HTML_Requester (object) :
                 assert form ['method'] == 'post'
                 break
         print self.nextfile
+        # FIXME: We may want to get the RSA parameters from the
+        #        javascript in the received html
+        enc = PW_Encode ()
         d = dict \
-            ( password      = self.args.password
+            ( password      = enc.encrypt (self.args.password)
             , userName      = self.args.username
             , selectedRealm = self.args.realm
             , loginType     = self.args.login_type
             , vpid_prefix   = self.args.vpid_prefix
+            , HeightData    = self.args.height_data
             )
         self.open (data = urlencode (d))
         print self.soup.prettify ()
@@ -62,6 +68,11 @@ def main () :
         ( '-H', '--host'
         , help    = 'Host part of URL default=%(default)s'
         , default = 'snx.lfrz.at'
+        )
+    cmd.add_argument \
+        ( '--height-data'
+        , help    = 'Height data in form, default empty'
+        , default = ''
         )
     cmd.add_argument \
         ( '-L', '--login-type'
@@ -87,20 +98,74 @@ def main () :
         , default = ''
         )
     args = cmd.parse_args ()
-    if 'username' not in args or 'password' not in args :
+    if not args.username or not args.password :
         n = netrc ()
         a = n.authenticators (args.host)
         if a :
             un, dummy, pw = a
-            if 'username' not in args :
-                args ['username'] = un
-            if 'password' not in args :
-                args ['password'] = pw
+            if not args.username :
+                args.username = un
+            if not args.password :
+                args.password = pw
         if 'password' not in args :
             password = getpass ('Password: ')
     rq = HTML_Requester (args)
     rq.login ()
 # end def main ()
+
+class PW_Encode (object) :
+    """ RSA encryption module with special padding and reversing to be
+        compatible with checkpoints implementation.
+        Test with non-random padding to get known value:
+        >>> p = PW_Encode (testing = True)
+        >>> print p.encrypt ('xyzzy')
+        451c2d5b491ee22d6f7cdc5a20f320914668f8e01337625dfb7e0917b16750cfbafe38bfcb68824b30d5cc558fa1c6d542ff12ac8e1085b7a9040f624ab39f625cabd77d1d024c111e42fede782e089400d2c9b1d6987c0005698178222e8500243f12762bebba841eae331d17b290f80bca6c3f8a49522fb926646c24db3627
+    """
+
+    def __init__ (self, m = None, e = None, testing = False) :
+        modulus = long \
+            ( 'c87e9e96ffde3ec47c3f116ea5ac0e15'
+              '34490b3da6dbbedae1af50dc32bf1012'
+              'bdb7e1ff67237e0302b48c8731f343ff'
+              '644662de2bb21d2b033127660e525d58'
+              '889f8f6f05744906dddc8f4b85e0916b'
+              '5d9cf5b87093ed260238674f143801b7'
+              'e58a18795adc9acefaf0f378326fea19'
+              '9ac6e5a88be83a52d4a77b3bba5f1aed'
+            , 16
+            )
+        exponent = long ('010001', 16)
+        m = m or modulus
+        e = e or exponent
+        self.pubkey  = RSA.construct ((m, e))
+        self.testing = testing
+    # end def __init__
+
+    def pad (self, txt) :
+        l = (self.pubkey.size () + 7) >> 3
+        r = []
+        r.append ('\0')
+        for x in reversed (txt) :
+            r.append (x)
+        r.append ('\0')
+        n = l - len (r) - 2
+        if self.testing :
+            r.append ('\1' * n)
+        else :
+            r.append (os.urandom (n))
+        r.append ('\x02')
+        r.append ('\x00')
+        return ''.join (reversed (r))
+    # end def pad
+
+    def encrypt (self, password) :
+        x = self.pad (password)
+        e = self.pubkey.encrypt (x, '')[0]
+        e = ''.join ('%02x' % ord (c) for c in reversed (e))
+        return e
+    # end def encrypt
+
+# end class PW_Encode
 
 if __name__ == '__main__' :
     main ()
